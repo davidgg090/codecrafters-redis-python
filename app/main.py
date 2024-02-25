3d26bb958dfd6885a3dd93ae23c9f1862694d5a5
david.gomez <davidgg090@gmail.com>
2024-02-25 16:29:08 -0300

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 import socket
+import threading
 
+
+def handle_client(conn):
+    while True:
+        data = conn.recv(1024)
+        if data == b"*1\r\n$4\r\nping\r\n":
+            res = "+PONG\r\n"
+            conn.send(res.encode())
+        elif not data.decode():
+            conn.close()
+            break
 
 def main():
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept()
     while True:
-        data = connection.recv(1024)
-        if not data:
-            break
-        connection.send(b"+PONG\r\n")
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        connection, _ = server_socket.accept()
+        print("Connected", connection)
+        print("Address", _)
+        threading.Thread(target=handle_client, args=(connection,)).start()
+
 
 
 if __name__ == "__main__":
