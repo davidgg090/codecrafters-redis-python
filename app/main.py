f00eaf5a723704f0ae4a05cb29f344ed8d889567
david.gomez <davidgg090@gmail.com>
2024-02-25 16:17:47 -0300

    fix Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept()
-    connection.send(b"+PONG\r\n")
     while True:
         data = connection.recv(1024)
         if not data:
