3bd562390ccb124687efbd77cc5f9efc5b558dd4
david.gomez <davidgg090@gmail.com>
2024-02-25 16:16:45 -0300

    Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept()
     connection.send(b"+PONG\r\n")
-
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        connection.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
